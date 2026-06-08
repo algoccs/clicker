48e55b59948e4a5e70a5d2679780177936f7b773
algoccs <algodemoacc@gmail.com>
2026-06-08 18:09:28 -0400

    Refactor main.py and add Picture class for images

--- main.py
+++ main.py
@@ -5,8 +5,6 @@ pygame.init()
 
 ANCHO, ALTO = 500, 500
 COLOR_FONDO = (64, 201, 144)
-COLOR_CARD = (227, 172, 54)
-COLOR_BORDER = (71, 11, 222)
 BLACK = (0,0,0)
 WHITE = (255,255,255)
 FPS = 60
@@ -18,16 +16,15 @@ reloj = pygame.time.Clock()
 class Area():
     def __init__(self, x, y, ancho, alto, color=None):
         self.rect = pygame.Rect(x, y, ancho, alto)
-        self.color = color
-    
+        if color:
+            self.color = color
+
+
     def fill(self):
         pygame.draw.rect(screen, self.color, self.rect)
 
     def change_color(self, new_color):
         self.color = new_color
-
-    def set_border(self, border_color, border_size):
-        pygame.draw.rect(screen, border_color, self.rect, border_size)
 
 class Label(Area):
     def set_text(self, text, size, text_color=BLACK):
@@ -37,21 +34,49 @@ class Label(Area):
         self.fill()
         screen.blit(self.image, (self.rect.x + dist_x, self.rect.y + dist_y))
 
-# CREANDO OBJETOS
-lista_cards = []
-x = 50
-for i in range(4):
-    card = Label(x, 50, 80, 100, COLOR_CARD)
-    card.set_text('Click!', 24)
-    lista_cards.append(card)
-    x += 110
+class Picture(Area):
+    def __init__(self, img_file, x, y, ancho, alto, color=None):
+        super().__init__(x, y, ancho, alto, color)
+        self.image = pygame.image.load(img_file) 
+
+    def draw(self):
+        screen.blit(self.image, (self.rect.x, self.rect.y))
+
+# OBJETOS:
+player = Picture('platform.png', 200, 400, 100, 20)
+ball = Picture('ball.png', (ANCHO - 50) // 2, 200, 50, 50)
+
+# LOGICA DE CREACION DE ENEMIGOS
+monsters = [] # Lista para los sprites
+inicio_x = 5
+pos_y = 5
+cantidad_enemigos = 9
+
+for fila in range(3):
+    pos_x = inicio_x
+    
+    for columna in range(cantidad_enemigos):
+        enemy = Picture('enemy.png', pos_x, pos_y, 50, 50)
+        monsters.append(enemy)
+        pos_x = pos_x + 55
+    # FINALIZAMOS LA PRIMERA FILA Y ASIGNAMOS PARAMETROS PARA LA 2DA
+    inicio_x = inicio_x + 35
+    cantidad_enemigos -= 1
+    pos_y = pos_y + 60
+
+
+finish = False
 
 while True:
     screen.fill(COLOR_FONDO)
 
-    for card in lista_cards:
-        card.draw(15, 30)
-        card.set_border(COLOR_BORDER, 5)
+    if not finish:
+        player.draw()
+        ball.draw()
+
+        for enemy in monsters:
+            enemy.draw()
+
 
     pygame.display.update()
     reloj.tick(FPS)
